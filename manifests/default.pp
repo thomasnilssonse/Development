0960ea51a0170ab1f187530559a1b5e1900b2785
unknown <nilsutho@WL2020415.eu.tieto.com>
2014-06-05 10:14:16 +0200

    Initial commit

---
+++ manifests/default.pp
@@ -0,0 +1,42 @@
+class must-have {
+  include apt
+  apt::ppa { "ppa:webupd8team/java": }
+
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
+    before => Apt::Ppa["ppa:webupd8team/java"],
+  }
+
+  exec { 'apt-get update 2':
+    command => '/usr/bin/apt-get update',
+    require => [ Apt::Ppa["ppa:webupd8team/java"], Package["git-core"] ],
+  }
+
+  package { ["vim",
+             "git-core",
+             "maven"]:
+    ensure => present,
+    require => Exec["apt-get update"],
+    before => Apt::Ppa["ppa:webupd8team/java"],
+  }
+
+  package { ["oracle-java7-installer"]:
+    ensure => present,
+    require => Exec["apt-get update 2"],
+  }
+
+  exec {
+    "accept_license":
+    command => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
+    cwd => "/home/vagrant",
+    user => "vagrant",
+    path    => "/usr/bin/:/bin/",
+    require => Package["curl"],
+    before => Package["oracle-java7-installer"],
+    logoutput => true,
+  }
+
+
+}
+
+include must-have
